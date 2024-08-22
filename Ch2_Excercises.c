5f997111c50d4a8d96a3ce17455c76eebfe057b4
Antony Youssef <107216599+antonyEng@users.noreply.github.com>
2024-08-22 21:04:05 +0200

    Add files via upload

---
+++ Ch2_Excercises.c
@@ -0,0 +1,76 @@
+/* ch 2 Ex 2 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("In C, lowercase letters are significant\n"
+            " main() is where program execution begins\n "
+           " opening and closing braces enclose program statements in a routine\n"
+           "All program statements must be terminated by a semicolon" );
+
+
+    return 0;
+}
+
+/* ch 2 Ex 3 */
+
+#include <stdio.h>
+int main (void)
+{
+printf ("Testing...");
+printf ("....1");
+printf ("...2");
+printf ("..3");
+printf ("\n");
+return 0;
+}
+
+/* ch 2 Ex 4 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int value1, value2, subtract;
+
+value1 = 15 ;
+value2 = 87 ;
+subtract = value1 - value2;
+    printf(" the subtract of the value %i from %i is %i\n", value1, value2, subtract);
+    return 0;
+}
+
+/* ch 2 Ex 5 */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+ {
+int sum ;
+/* COMPUTE RESULT */
+sum = 25 + 37 - 19 ;
+// DISPLAY RESULTS
+printf ("The answer is %i\n", sum);
+return 0;
+ }
+
+
+
+/* ch 2 Ex 6 */
+#include <stdio.h>
+int main (void)
+{
+int answer, result;
+answer = 100;
+result = answer - 10;
+printf ("The result is %i\n", result + 5);
+return 0;
+}
+
+
+
+
+
