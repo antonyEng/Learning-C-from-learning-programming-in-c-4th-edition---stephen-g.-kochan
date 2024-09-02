5207af5ca397b3a4a7b58706cb1d84dad2ec5df8
Antony Youssef <107216599+antonyEng@users.noreply.github.com>
2024-09-02 15:29:09 +0200

    Add files via upload

---
+++ several_files/library.h
@@ -0,0 +1,7 @@
+#ifndef LIBRARY_H_INCLUDED
+#define LIBRARY_H_INCLUDED
+
+int add (int x,int y);
+int sub (int x,int y);
+
+#endif // LIBRARY_H_INCLUDED
---
+++ several_files/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "library.h"
+int main()
+{
+    printf("%d + %d = %d \n",4,5,add(4,5) );
+    return 0;
+}
